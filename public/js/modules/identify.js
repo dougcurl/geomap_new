34052aa2d1d3703dd5139943542c80f6a05978e4
dcurl <doug@uky.edu>
2026-03-13 14:58:00 -0400

    updates

--- public/js/modules/identify.js
+++ public/js/modules/identify.js
@@ -1,32 +1,65 @@
-export async function initializeIdentify(view) {
-    // Set up click event handler
-    view.on("click", (event) => {
-      executeIdentify(event, view);
-    });
-  }
-  
-  async function executeIdentify(event, view) {
-    try {
-      const response = await view.hitTest(event);
-      const features = response.results?.map(result => result.graphic);
-  
-      if (features?.length > 0) {
-        // Create popup content for each feature
-        view.popup.open({
-          features: features,
-          location: event.mapPoint
-        });
-      } else {
-        // No features found at click location
-        view.popup.close();
-      }
-    } catch (error) {
-      console.error("Error in identify:", error);
-      view.popup.close();
-    }
-  }
+import { identify } from 'https://js.arcgis.com/5.0/@arcgis/core/rest/identify.js';
+import IdentifyParameters from 'https://js.arcgis.com/5.0/@arcgis/core/rest/support/IdentifyParameters.js';
 
-  
-export function identifyFeatures(layer, mapPoint, view) {
-  // Your identify logic here
+export async function initializeIdentify(view) {
+    view.on("click", (event) => {
+        executeIdentify(event, view);
+    });
+}
+
+async function executeIdentify(event, view) {
+    try {
+        // First try hitTest for FeatureLayers
+        const hitResponse = await view.hitTest(event);
+        const hitFeatures = hitResponse.results
+            ?.filter(r => r.graphic?.layer?.type === 'feature')
+            ?.map(r => r.graphic);
+
+        if (hitFeatures?.length > 0) {
+            view.openPopup({
+                features: hitFeatures,
+                location: event.mapPoint
+            });
+            return;
+        }
+
+        // For MapImageLayers, use esri/rest/identify
+        const identifyPromises = [];
+
+        view.map.allLayers.forEach(layer => {
+            if (layer.type === 'map-image' && layer.visible) {
+                const params = new IdentifyParameters({
+                    geometry: event.mapPoint,
+                    mapExtent: view.extent,
+                    width: view.width,
+                    height: view.height,
+                    tolerance: 3,
+                    returnGeometry: true,
+                    layerOption: 'visible'
+                });
+
+                identifyPromises.push(
+                    identify(layer.url, params)
+                        .then(result => result.results?.map(r => r.feature) ?? [])
+                        .catch(() => [])
+                );
+            }
+        });
+
+        const results = await Promise.all(identifyPromises);
+        const allFeatures = results.flat();
+
+        if (allFeatures.length > 0) {
+            view.openPopup({
+                features: allFeatures,
+                location: event.mapPoint,
+                title: "Identify Results"
+            });
+        } else {
+            view.closePopup();
+        }
+
+    } catch (error) {
+        console.error("Error in identify:", error);
+    }
 }
